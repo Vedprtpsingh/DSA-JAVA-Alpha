13d746b9f0daa7d8fdced728ebeb6b447fe7a614
VED PRATAP SINGH <119805462+Vedprtpsingh@users.noreply.github.com>
2026-05-18 08:57:30 +0530

    dp

--- 29DP/_01FibonacciUsingMemorization.java
+++ 29DP/_01FibonacciUsingMemorization.java
@@ -1,7 +1,7 @@
 public class _01FibonacciUsingMemorization {
         public static int fib(int n, int dp[]) {
         if (n == 1 || n == 0) {
-            return n;
+            return n;   
         }
         if (dp[n] != 0) {
             return dp[n];
