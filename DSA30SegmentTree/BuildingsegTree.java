2643e0b4e75bacb91e7001512ca3a44e222d2bd3
VED PRATAP SINGH <119805462+Vedprtpsingh@users.noreply.github.com>
2026-07-23 23:39:29 +0530

    Update BuildingsegTree.java

--- DSA30SegmentTree/BuildingsegTree.java
+++ DSA30SegmentTree/BuildingsegTree.java
@@ -14,6 +14,36 @@ class BuildingsegTree {
         tree[i] = tree[2 * i + 1] + tree[2 * i + 2];
         return tree[i];
     }
+
+    public static int getSum(int arr[], int qi, int qj) {
+    int n = arr.length;
+    return getSumUtil(0, 0, n - 1, qi, qj);
+}   
+
+    public static int getSumUtil(int i, int si, int sj, int qi, int qj) {
+
+    // No Overlap
+    if (qj < si || qi > sj) {
+        return 0;
+    }
+
+    // Complete Overlap
+    else if (si >= qi && sj <= qj) {
+        return tree[i];
+    }
+
+    // Partial Overlap
+    else {
+        int mid = (si + sj) / 2;
+
+        int left = getSumUtil(2 * i + 1, si, mid, qi, qj);
+        int right = getSumUtil(2 * i + 2, mid + 1, sj, qi, qj);
+
+        return left + right;
+    }
+}
+
+                
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,5,6,7,8};
         init(arr.length);
